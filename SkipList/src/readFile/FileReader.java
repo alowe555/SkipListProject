8273ac40219673c8bdee1a3ffc65c63f0de64801
alowe555 <78287445+alowe555@users.noreply.github.com>
2021-09-02 13:07:38 -0400

    Added File Structure

---
+++ SkipList/src/readFile/FileReader.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package readFile;
+
+/**
+ * @author alowe
+ *
+ */
+public class FileReader {
+
+    /**
+     * 
+     */
+    public FileReader() {
+        // TODO Auto-generated constructor stub
+    }
+
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
